b2999ea79cc3cd9a196a68bbfbc1b671b7322ca9
deepeshrohilla <50067114+deepeshrohilla@users.noreply.github.com>
2020-11-03 18:45:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function getAverageColor(imageElement, ratio) {
+    const canvas = document.createElement("canvas")
+
+    let height = canvas.height = imageElement.naturalHeight
+    let width = canvas.width = imageElement.naturalWidth
+
+    const context = canvas.getContext("2d")
+    context.drawImage(imageElement, 0, 0)
+
+    let data, length
+    let i = -4,
+        count = 0
+
+    try {
+        data = context.getImageData(0, 0, width, height)
+        length = data.data.length
+    } catch (err) {
+        console.error(err)
+        return {
+            R: 0,
+            G: 0,
+            B: 0
+        }
+    }
+    let R, G, B
+    R = G = B = 0
+
+    while ((i += ratio * 4) < length) {
+        ++count
+
+        R += data.data[i]
+        G += data.data[i + 1]
+        B += data.data[i + 2]
+    }
+
+    R = ~~(R / count)
+    G = ~~(G / count)
+    B = ~~(B / count)
+
+    return {
+        R,
+        G,
+        B
+    }
+}
+
+const image = document.querySelector("img")
+image.onload = () => {
+    const { R, G, B } = getAverageColor(image, 4)
+
+    document.body.style.background = `rgb(${R}, ${G},${B})`
+}
